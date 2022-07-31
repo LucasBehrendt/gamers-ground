8b78f4ae10ce61e1d8dcb7d3752c2408e4ddefe2
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-07-31 22:36:24 +0000

    Create home page template, url & view

--- gamers_ground/settings.py
+++ gamers_ground/settings.py
@@ -68,7 +68,10 @@ ROOT_URLCONF = 'gamers_ground.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class Home(generic.TemplateView):
+    """Home page"""
+    template_name = 'home/index.html'
