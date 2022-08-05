6db16ad4e505404cb403b4e4bcfad1c94ee04a76
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-05 23:02:02 +0000

    Create product list template & render products

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductList.as_view(), name='Products'),
+    path('', views.ProductList.as_view(), name='products'),
 ]
