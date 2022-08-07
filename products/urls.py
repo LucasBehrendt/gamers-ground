178b5854c2cb3474763bb7d27585d52c9e4f8fad
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-07 18:32:33 +0000

    Add view & url for product detail

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductList.as_view(), name='products'),
+    path('<category>/<slug>/',
+         views.ProductDetail.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Product
+from .models import Product, Category
 
 
 class ProductList(generic.ListView):
     """
     Main product list view, renders a list of all products.
+    """
+    model = Product
+
+
+class ProductDetail(generic.DetailView):
+    """
+    Main product detail view, renders a specific product.
     """
     model = Product
