c136c82a6ea7e272af9e1b7260bca488348a3651
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-09 17:09:14 +0000

    Create view, url & template for cart page

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class Cart(generic.TemplateView):
+    """Renders the cart page with saved items"""
+    template_name = 'cart/cart.html'
