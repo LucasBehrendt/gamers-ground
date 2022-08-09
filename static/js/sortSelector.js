345f586fdbfcfb318612eaf9fcb1aad59ec5a7f7
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-09 01:20:58 +0000

    Add js to sort products with select menu

---
+++ static/js/sortSelector.js
@@ -0,0 +1,20 @@
+document.getElementById('sort-selector').onchange = function() {
+    let selector = this.value;
+    let currentUrl = new URL(window.location);
+
+    let selectedValue = selector;
+    if (selectedValue != 'reset') {
+        let sort = selectedValue.split('_')[0];
+        let direction = selectedValue.split('_')[1];
+
+        currentUrl.searchParams.set('sort', sort);
+        currentUrl.searchParams.set('direction', direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete('sort');
+        currentUrl.searchParams.delete('direction');
+
+        window.location.replace(currentUrl);
+    }
+}
