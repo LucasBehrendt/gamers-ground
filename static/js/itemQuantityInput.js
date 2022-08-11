c64e0f7df7e65f5dff954c7bf62af3cf24d88101
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-11 00:37:12 +0000

    Fix issue with updating item quantity

--- static/js/itemQuantityInput.js
+++ static/js/itemQuantityInput.js
@@ -1,24 +1,27 @@
  // Decrement quantity button
- document.querySelector('.decrement-qty').onclick = function(event) {
-    event.preventDefault();
-    let closestInput = this.closest('.input-group').querySelector('.qty-input');
-    let currentValue = parseInt(closestInput.value);
-    // if (currentValue > closestInput.getAttribute('min')) {
-    //     closestInput.value = currentValue - 1;
-    // }
-    closestInput.value = currentValue - 1;
-    let form = document.getElementById('update-qty-form')
-    // form.submit()
-}
+ document.querySelectorAll('.decrement-qty').forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        let closestInput = button.closest('.input-group').querySelector('.qty-input');
+        let currentValue = parseInt(closestInput.value);
+        if (currentValue > closestInput.getAttribute('min')) {
+            closestInput.value = currentValue - 1;
+        }
+        let form = document.getElementById('update-qty-form');
+        // form.submit();
+    });
+});
 
 // Increment quantity button
-document.querySelector('.increment-qty').onclick = function(event) {
-    event.preventDefault();
-    let closestInput = this.closest('.input-group').querySelector('.qty-input');
-    let currentValue = parseInt(closestInput.value);
-    if (currentValue < closestInput.getAttribute('max')) {
-        closestInput.value = currentValue + 1;
-    }
-    let form = document.getElementById('update-qty-form')
-    // form.submit()
-}
+document.querySelectorAll('.increment-qty').forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        let closestInput = button.closest('.input-group').querySelector('.qty-input');
+        let currentValue = parseInt(closestInput.value);
+        if (currentValue < closestInput.getAttribute('max')) {
+            closestInput.value = currentValue + 1;
+        }
+        let form = document.getElementById('update-qty-form');
+        // form.submit();
+    });
+});
