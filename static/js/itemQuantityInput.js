7aa1ea638e608fdb7c76b4e9e2a8584ee2ead942
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-11 02:10:57 +0000

    Add logic for manual input update on item quantity

--- static/js/itemQuantityInput.js
+++ static/js/itemQuantityInput.js
@@ -2,13 +2,15 @@
  document.querySelectorAll('.decrement-qty').forEach((button) => {
     button.addEventListener('click', (event) => {
         event.preventDefault();
+        let itemId = button.getAttribute('data-item_id');
         let closestInput = button.closest('.input-group').querySelector('.qty-input');
         let currentValue = parseInt(closestInput.value);
-        if (currentValue > closestInput.getAttribute('min')) {
-            closestInput.value = currentValue - 1;
-        }
-        let form = document.getElementById('update-qty-form');
-        // form.submit();
+        // if (currentValue > closestInput.getAttribute('min')) {
+        //     closestInput.value = currentValue - 1;
+        // }
+        closestInput.value = currentValue - 1;
+        let form = document.getElementById(`update-qty-form-${itemId}`);
+        form.submit();
     });
 });
 
@@ -16,12 +18,31 @@
 document.querySelectorAll('.increment-qty').forEach((button) => {
     button.addEventListener('click', (event) => {
         event.preventDefault();
+        let itemId = button.getAttribute('data-item_id');
         let closestInput = button.closest('.input-group').querySelector('.qty-input');
         let currentValue = parseInt(closestInput.value);
         if (currentValue < closestInput.getAttribute('max')) {
             closestInput.value = currentValue + 1;
         }
-        let form = document.getElementById('update-qty-form');
-        // form.submit();
+        let form = document.getElementById(`update-qty-form-${itemId}`);
+        form.submit();
     });
+});
+
+// update manual input in quantity form
+let qtyInputs = document.querySelectorAll('.qty-input').forEach((itemQty) => {
+    let currentValue = itemQty.value;
+    itemQty.onchange = function() {
+        if (parseInt(itemQty.value) || itemQty.value == 0) {
+            if (parseInt(itemQty.value) >= 0 && parseInt(itemQty.value) <= parseInt(itemQty.getAttribute('max'))) {
+                let itemId = itemQty.getAttribute('data-item_id');
+                let form = document.getElementById(`update-qty-form-${itemId}`);
+                form.submit();
+            } else {
+                itemQty.value = currentValue;
+            }
+        } else {
+            itemQty.value = currentValue;
+        }
+    }
 });
