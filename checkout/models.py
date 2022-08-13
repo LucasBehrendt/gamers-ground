c4e774f7f1c639f3d547a91fa55a1939577fbc53
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-13 22:24:44 +0000

    Create order model in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,63 @@
+from uuid import uuid4
+from decimal import Decimal, ROUND_DOWN
 from django.db import models
+from django.conf import settings
+from django_countries.fields import CountryField
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    """Main order model"""
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=False, blank=False)
+    address1 = models.CharField(max_length=80, null=False, blank=False)
+    address2 = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(
+        max_digits=3, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        '''
+        Generate a random, unique order number using UUID.
+        Taken from Boutique Ado walkthrough project.
+        '''
+        return uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        Taken from Boutique Ado walkthrough project.
+        """
+        self.order_total = self.lineitems.aggregate(
+            models.Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = Decimal(7.99).quantize(
+                Decimal('.01'), rounding=ROUND_DOWN)
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override original save method to
+        set generated order number.
+        Taken from Boutique Ado walkthrough project.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
