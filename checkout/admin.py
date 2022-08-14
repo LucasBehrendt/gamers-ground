ac874b56859b9ee981db081352ddda5ad82c58a1
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-14 21:05:52 +0000

    Modify field names in order model

--- checkout/admin.py
+++ checkout/admin.py
@@ -20,17 +20,17 @@ class OrderAdmin(admin.ModelAdmin):
         'order_total', 'grand_total')
 
     fields = (
-        'order_number', 'date', 'first_name', 'last_name', 'email', 'phone',
-        'address1', 'address2', 'postcode', 'city', 'country',
+        'order_number', 'date', 'first_name', 'last_name', 'email_address', 'phone_number',
+        'street_address_1', 'street_address_2', 'postcode', 'city', 'country',
         'delivery_cost', 'order_total', 'grand_total')
 
     list_display = (
-        'order_number', 'date', 'first_name', 'last_name', 'email',
+        'order_number', 'date', 'first_name', 'last_name', 'email_address',
         'delivery_cost', 'order_total', 'grand_total')
 
     search_fields = (
-        'order_number', 'date', 'first_name', 'last_name', 'email',
-        'address1', 'address2', 'postcode', 'city', 'country',
+        'order_number', 'date', 'first_name', 'last_name', 'email_address',
+        'street_address_1', 'street_address_2', 'postcode', 'city', 'country',
         'delivery_cost', 'order_total', 'grand_total')
 
     ordering = ('-date',)
