c2f5908ac6190bd629c84a8b3b51610a5aad4ae7
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-14 01:00:19 +0000

    Create view & url for checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from django.contrib import messages
+from .models import Order
 
-# Create your views here.
+
+class Checkout(generic.CreateView):
+    """Renders checkout page with payment form"""
+    model = Order
+    fields = (
+        'first_name', 'last_name', 'email', 'phone', 'address1',
+        'address2', 'postcode', 'city', 'country')
+    template_name = 'checkout/checkout.html'
+    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(
+                request, 'There is nothing in you cart at the moment.')
+            return redirect('products')
+        return super().get(request, *args, **kwargs)
