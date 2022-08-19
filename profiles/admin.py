68e7f23f7b06e2337f8176d2b3742dd7ae766767
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-19 23:23:06 +0000

    Modify names on profile model fields & admin config

--- profiles/admin.py
+++ profiles/admin.py
@@ -8,13 +8,13 @@ class UserProfileAdmin(admin.ModelAdmin):
     Configuration for user profiles on admin page.
     """
     fields = (
-        'user', 'default_phone_number', 'default_street_address_1',
-        'default_street_address_2', 'default_postcode', 'default_city',
-        'default_country')
+        'user', 'phone_number', 'street_address_1',
+        'street_address_2', 'postcode', 'city',
+        'country')
 
     list_display = ('user',)
 
     search_fields = (
-        'user__username', 'default_phone_number', 'default_street_address_1',
-        'default_street_address_2', 'default_postcode', 'default_city',
-        'default_country')
+        'user__username', 'phone_number', 'street_address_1',
+        'street_address_2', 'postcode', 'city',
+        'country')
