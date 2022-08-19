cce495ba3c5220d1c970990dc5b2ca374315e427
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-19 02:02:15 +0000

    Create view, url & template for profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import UserProfile
 
-# Create your views here.
+
+class Profile(generic.TemplateView):
+    """Dispay user profile page"""
+    template_name = 'profiles/profile.html'
