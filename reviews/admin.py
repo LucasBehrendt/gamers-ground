cbece1f83a320ee26b21fa9e8c081a5a2521af76
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-22 21:30:45 +0000

    Create review model & register on admin page

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Configuration for reviews on admin page.
+    """
+    list_filter = ('created_on',)
+    list_display = ('user', 'product', 'rating', 'created_on')
+    search_fields = ('user__username', 'rating', 'review', 'product__name')
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+rating_choices = (
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+)
+
+
+class Review(models.Model):
+    """Main model for leaving reviews & ratings on products"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=rating_choices, default=5)
+    review = models.TextField(max_length=500, null=False, blank=False)
+    created_on = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.user} reviewed {self.product} | Rating: {self.rating}'
