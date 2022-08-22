7dfdcfd6ed755a4f3d1b8aea7227206ad92d51d0
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-08-22 13:57:56 +0000

    Add password change link & login required to profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from checkout.models import Order
 from .models import UserProfile
 from .forms import UserDetailsForm, UserDeliveryForm
 
 
+@login_required
 def profile(request):
     """user profiles"""
     profile = get_object_or_404(UserProfile, user=request.user)
